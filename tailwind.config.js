3156d1e840a1567a9e829dd4769311534bb0a38a
Heba Mamdooh <52792349+HebaMamdooh@users.noreply.github.com>
2021-12-28 16:54:19 +0200

    adding the 4 plugins for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
